742d11cfcdff659e101115c5ac210721292ad326
JonathanMielke <134979105+JonathanMielke@users.noreply.github.com>
2024-12-03 01:34:27 -0700

    Start work on part2 of day 2 python

--- day_2/python/main.py
+++ day_2/python/main.py
@@ -11,7 +11,7 @@ def format_input(input_text_file: str) -> list[list[int]]:
 def get_safe_reports_number(reports_formatted: list[list[int]]) -> int:
     safe_reports_number = 0
     for report in reports_formatted:
-        safe = True
+        error_count = 0
         is_increasing = None
         for i, number in enumerate(report):
             if i != 0:
@@ -21,21 +21,22 @@ def get_safe_reports_number(reports_formatted: list[list[int]]) -> int:
                 if report[i - 1] - number < 0:
                     is_increasing = True
 
-                if is_increasing != previous_is_increasing and previous_is_increasing is not None:
-                    safe = False
                 increase = abs(report[i - 1] - number)
+                if is_increasing != previous_is_increasing and previous_is_increasing is not None:
+                    error_count += 1
 
                 if increase < 1 or increase > 3:
-                    safe = False
-
-        if safe:
+                    error_count += 1
+        print(error_count)
+        if error_count < 2:
+            print(report)
             safe_reports_number += 1
 
     return safe_reports_number
 
 
 def main():
-    print(f"This many reports are safe: {get_safe_reports_number(format_input('./input.txt'))} ")
+    print(f"This many reports are safe: {get_safe_reports_number(format_input('./input_example.txt'))} ")
 
 
 if __name__ == '__main__':
